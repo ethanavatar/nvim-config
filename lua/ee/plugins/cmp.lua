4326072d85e82bafd91c4cb032c8acdf2b726c10
Ethan Evans <ethanalexevans@gmail.com>
2024-04-15 20:54:46 -0700

    a bunch of greatly needed keybind changes

--- lua/ee/plugins/cmp.lua
+++ lua/ee/plugins/cmp.lua
@@ -38,7 +38,7 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete({}),
-                ['<CR>'] = cmp.mapping.confirm({
+                ['<A-y>'] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = false,
                 }),
